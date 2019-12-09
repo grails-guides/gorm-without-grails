d77a8565c4fa37cc2fbfc63878aeeefcf8ec4255
Sergio del Amo <sergio.delamo@softamo.com>
2019-12-09 07:23:41 +0100

    move save to GORM data service

--- complete/src/main/groovy/demo/init/BootStrap.groovy
+++ complete/src/main/groovy/demo/init/BootStrap.groovy
@@ -2,23 +2,28 @@ package demo.init
 
 import demo.domain.Manufacturer
 import demo.domain.Vehicle
+import demo.service.ManufacturerService
 import grails.gorm.transactions.Transactional
 import groovy.transform.CompileStatic
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 @Component
 @CompileStatic
 class BootStrap {
 
+    @Autowired
+    ManufacturerService manufacturerService
+
     @Transactional
     void init() {
         Manufacturer audi = new Manufacturer(name: 'audi')
         audi.addToVehicles(new Vehicle(name: 'A3', year: 1996))
         audi.addToVehicles(new Vehicle(name: 'A4', year: 1994))
-        audi.save()
+        manufacturerService.save(audi)
 
         Manufacturer ford = new Manufacturer(name: 'ford')
         ford.addToVehicles(new Vehicle(name: 'Ford KA', year: 1996))
-        ford.save()
+        manufacturerService.save(ford)
     }
 }
--- complete/src/main/groovy/demo/service/ManufacturerService.groovy
+++ complete/src/main/groovy/demo/service/ManufacturerService.groovy
@@ -1,6 +1,8 @@
 package demo.service
 
 import demo.domain.Manufacturer
+import grails.gorm.transactions.ReadOnly
+import grails.gorm.transactions.Transactional
 import groovy.transform.CompileStatic
 import org.springframework.stereotype.Service
 
@@ -8,5 +10,8 @@ import org.springframework.stereotype.Service
 @grails.gorm.services.Service(Manufacturer)
 @Service
 interface ManufacturerService {
+
     List<Manufacturer> findAll()
+
+    Manufacturer save(Manufacturer manufacturer)
 }
